d2954be977ea964b428d4202876da6609b14527d
Luca Ferro <lferro@c1r11s3.42lausanne.ch>
2023-10-25 18:41:05 +0200

    init repo

---
+++ get_next_line.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lferro <lferro@student.42lausanne.ch>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 17:39:52 by lferro            #+#    #+#             */
+/*   Updated: 2023/10/25 18:38:38 by lferro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+#include <fcntl.h>
+
+
+char	*get_next_line(int fd)
+{
+	void	*buf;
+
+
+	read(fd, buf, )
+
+}
+
+int main(int argc, char const *argv[])
+{
+
+	fd = open("file.txt", O_RDONLY);
+	int	fd;
+
+	return 0;
+}
